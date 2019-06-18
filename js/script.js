64c11283f5cad2f0c8d767c25e8121f659f9a101
germancardenasm <germancardenas@gmail.com>
2019-06-18 10:45:47 -0500

    see all characters button added with fecth() working, also renders the first page. Now adding the pagination

--- js/script.js
+++ js/script.js
@@ -1,16 +1,48 @@
 
 console.log('init request');
 const QTY_HOME_CHARACTERS = 3;
-const showAllButton = document.getElementById('show-all');
+let TOTAL_CHARACTERS = 0;
+let TOTAL_PAGES=0;
+let nextPage = '';
+let previusPage = '';
 const charactersHomeContainer = document.getElementById('characters-home-div');
+const showAllSection = document.getElementById('showAllSection');
+const showAllButton = document.getElementById('button-showAll');
 
+bootstrap()
+renderCharactersContainers();
+showAllButton.addEventListener('click', showAllCharacters);
+const homeCharacters =  getRandomCharacters(QTY_HOME_CHARACTERS);
+Promise.all(homeCharacters).then(data => renderCharacters(data))
 
+function bootstrap(){
+  const apiInfo = getCharacter();
+  apiInfo.then( data =>  {
+    TOTAL_CHARACTERS = data.info.count;
+    TOTAL_PAGES = data.info.pages;
+  })
+}
 
-render();
+function showAllCharacters(event){
+  event.preventDefault();
+  const requestCharactersList = getCharacter();
+  requestCharactersList.then(receivedData => {
+    removeAllCharacters();
+    renderCharactersContainers(receivedData.results.length);
+    renderCharacters(receivedData.results);
+  });
+  showAllSection.classList.add('d-none')
+}
 
-function render(){
+function removeAllCharacters(){
+  var myNode = document.getElementById('characters-home-div');
+  while (myNode.firstChild) {
+    myNode.removeChild(myNode.firstChild);
+  }
+}
 
-  for(let i = 0; i < QTY_HOME_CHARACTERS; i++){
+function renderCharactersContainers(qtyOfCharacters=QTY_HOME_CHARACTERS){
+  for(let i = 0; i < qtyOfCharacters; i++){
     const container = document.createElement('div');
     const nameContainer = document.createElement('div');
     const img = document.createElement('img');
@@ -25,8 +57,6 @@ function render(){
     container.append(nameContainer);
     charactersHomeContainer.appendChild(container);
   }
-  const homeCharacters =  getRandomCharacters(QTY_HOME_CHARACTERS);
-  Promise.all(homeCharacters).then(data => renderCharacters(data))
 }
 
 
@@ -37,19 +67,18 @@ function renderCharacters(charactersArray){
     container.children[0].alt = element.name + ' image';
     container.children[1].innerHTML = element.name;
   });
-  console.dir(charactersArray)
 }
 
 function getRandomCharacters(qtyOfCharacter){
   let promises = [];
   for(let i = 0; i < qtyOfCharacter; i++){
-    promises.push(getCharacter(randomNumber(400)))
+    promises.push(getCharacter(randomNumber(493)))
   }
   return promises;
 }  
 
-function getCharacter(characterId){
-   return fetch('https://rickandmortyapi.com/api/character/'+characterId)
+function getCharacter(characterId = ''){
+   return fetch('https://rickandmortyapi.com/api/character/' + characterId)
             .then(function(response) {
               return response.json();
             })
@@ -57,6 +86,7 @@ function getCharacter(characterId){
               return response;
             })
 }
+
 
 function randomNumber(maxLimit){
   return Math.floor(Math.random()*maxLimit) + 1;
